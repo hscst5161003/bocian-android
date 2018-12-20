b4209f24049e2aa86104357b7e6bbaf49a7b1762
unknown <5161003@sophia.local>
2018-12-20 11:58:46 +0900

    出欠通知の作成

--- app/src/main/java/com/websarva/wings/android/bocian/model/AttendanceNotice.java
+++ app/src/main/java/com/websarva/wings/android/bocian/model/AttendanceNotice.java
@@ -1,4 +1,12 @@
 package com.websarva.wings.android.bocian.model;
 
 public class AttendanceNotice {
+    private String participantName;
+    private AttendanceState attendanceState;
+
+
+    public enum AttendanceState {
+        PARTICIPATE,
+        NOT_ATTENDING,
+    }
 }
