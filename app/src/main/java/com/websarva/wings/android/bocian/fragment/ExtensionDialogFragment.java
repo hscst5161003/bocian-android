76230755a96c947c014eafdf8497972449ad0c49
ashog <38340789+5161025@users.noreply.github.com>
2018-12-29 03:05:21 +0900

    Date型 → LocalDate または LocalTime型へ変更 Calenderクラスはうるう年を考慮していない為 また二つの時間の計算もDateTimeAPIなら簡易になるため変更した

---
+++ app/src/main/java/com/websarva/wings/android/bocian/fragment/ExtensionDialogFragment.java
@@ -0,0 +1,46 @@
+package com.websarva.wings.android.bocian.fragment;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.widget.Spinner;
+
+import com.websarva.wings.android.bocian.R;
+
+public class ExtensionDialogFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity() , R.style.AlertDialogStyle);
+        //延長画面からダイアログに時、分をStringで取得(メッセージで使用)
+        Spinner hour = getActivity().findViewById(R.id.extension_spinar_hour);
+        String hourSelectedItem = hour.getSelectedItem().toString();
+        Spinner minutes = getActivity().findViewById(R.id.extension_spinar_minutes);
+        String minutesSlectedItem = minutes.getSelectedItem().toString();
+
+        //ダイアログのタイトルをセット
+        builder.setTitle("延長確認");
+        //ダイアログのメッセージをセット
+        builder.setMessage(hourSelectedItem + "時" + minutesSlectedItem + "分" + "まで延長します。よろしいですか？");
+
+
+        builder.setPositiveButton(R.string.bt_confirm, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setNegativeButton(R.string.bt_cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        return builder.create();
+    }
+}
